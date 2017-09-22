6044fdc4d504ab7dbae804b5b1477673eb876b41
SidneyNemzer <sidney@ucode.com>
2017-09-21 22:24:56 -0700

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -51,7 +51,8 @@ const config = {
   },
 
   devServer: {
-    contentBase: './build',
+    contentBase: './public',
+    historyApiFallback: true,
     inline: true,
     stats: {
       colors: true
@@ -85,57 +86,3 @@ const generateConfig = (baseConfig, pagesToAdd) => {
 }
 
 module.exports = generateConfig(config, pages);
-
-/* This is a backup config, you can use this if the config generation fails
-module.exports = {
-  entry: {
-    'elm/index': [
-      './src/index.js'
-    ]
-  },
-  output: {
-    path: path.resolve('./build/'),
-    filename: '[name].js'
-  },
-
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Home',
-      template: 'src/html/index.ejs'
-    })
-  ],
-
-  module: {
-    rules: [
-      {
-        test: /\.(css|scss)$/,
-        use: [
-          'style-loader',
-          'css-loader',
-        ]
-      },
-      {
-        test:    /\.html$/,
-        exclude: /node_modules/,
-        loader:  'file-loader?name=[name].[ext]',
-      },
-      {
-        test:    /\.elm$/,
-        exclude: [/elm-stuff/, /node_modules/],
-        loader:  'elm-webpack-loader?verbose=true&warn=true',
-      }
-    ],
-
-    noParse: /\.elm$/,
-  },
-
-  // Setup webpack-dev-server
-  devServer: {
-    contentBase: './build',
-    inline: true,
-    stats: {
-      colors: true
-    }
-  }
-}
-*/
